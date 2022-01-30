6cc9f7d40a557c3437f50ceb6ba4365a5cff0872
Monakus <monakus00@gmail.com>
2022-01-30 17:21:02 +0100

    Rewrote the tests as Value-Parametrized tests to avoid code (tests) duplication.

--- src/selectionSortStrategy.cpp
+++ src/selectionSortStrategy.cpp
@@ -20,26 +20,3 @@ int SelectionSortStrategy::returnIndexOfSmallerValue(int val1, int index1, int v
         return index1;
     return index2;
 }
-
-//class SelectionSortStrategy : public SortingStrategy{
-//public:
-//    void sort(int array[], int count) override{
-//        for (int i = 0; i < count - 1; ++i) {
-//            int smallestIndex = findSmallestElementIndex(array, i, count);
-//            swap(array[i], array[smallestIndex]);
-//        }
-//    }
-//private:
-//    int findSmallestElementIndex(const int array[], int start, int count){
-//        int smallestElementIndex = start;
-//        for (int i = start; i < count; ++i)
-//            smallestElementIndex = returnIndexOfSmallerValue(array[i], i, array[smallestElementIndex], smallestElementIndex);
-//        return smallestElementIndex;
-//    }
-//
-//    int returnIndexOfSmallerValue(int val1, int index1, int val2, int index2){
-//        if (val1 < val2)
-//            return index1;
-//        return index2;
-//    }
-//};
